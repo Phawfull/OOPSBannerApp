5e0bc6da0458e03aa126ebaed6b19bf9ac34342a
Phawfull <arush.g.2007@gmail.com>
2026-03-10 13:06:26 +0530

    UC5 DONE

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,17 @@
-
 public class OOPSBannerApp {
-    public static void main(String args[]) {
-        String lines [] = new String[7];
+public static void main(String[] args) {
 
-        lines[0] = String.join(" ", "  ***  ", "    ***  ", " *****", "  ***** ");
-        lines[1] = String.join(" ", " **  **", "  **  **", "  **  **", " **     ");
-        lines[2] = String.join(" ", "**    **", "**    **", " **  **", " **     ");
-        lines[3] = String.join(" ", "**    **", "**    **", " *****", "  ***** ");
-        lines[4] = String.join(" ", "**    **", "**    **", " **", "       **");
-        lines[5] = String.join(" ", " **  **", "  **  **", "  **", "  **   **");
-        lines[6] = String.join(" ", "  ***  ", "   ***  ", "  **", "   ***** ");
+        String[] banner = {
+                String.join("   ", " ***** ", " ***** ", " ******", " ******"),
+                String.join("   ", "*     *", "*     *", "*     *", "*     "),
+                String.join("   ", "*     *", "*     *", "*     *", "*     "),
+                String.join("   ", "*     *", "*     *", " ******", " ***** "),
+                String.join("   ", "*     *", "*     *", "*      ", "      *"),
+                String.join("   ", "*     *", "*     *", "*      ", "      *"),
+                String.join("   ", " ***** ", " ***** ", "*      ", " ******")
+        };
 
-        for (String line : lines) {
+        for (String line : banner) {
             System.out.println(line);
         }
     }
